a255398fa56e4ed953b462d324142f204db90348
Dumisani <chikomod@brea.udu>
2025-03-19 16:37:07 -0400

    interview

---
+++ chikomod.py
@@ -0,0 +1,17 @@
+def letter_count(s):
+    print(s)
+    unique_l =""
+    for letter in s:
+        print(letter)
+
+     if letter not in unique_l():
+         unique_l  += ""
+         return unique_l
+
+
+
+
+def main():
+    easy = letter_count("dumi")
+
+main()
